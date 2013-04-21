054b5934d0c8dbbe8a96ab3f58879325b819985b
Preston Timmons <prestontimmons@gmail.com>
2013-04-21 13:56:47 -0500

    Updated README and setup.py.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-Route urls to file system templates in Django.
+# Route urls to file system templates in Django.
+
+Set up a url:
+
+```python
+from filepages.urls import filepages_urlpatterns
+
+urlpatterns += filepages_urlpatterns(
+    directory="filepages/",
+    name="filepages",
+)
+```
+
+Add a template: `filepages/mytemplate.html`.
+
+Load the page at `/mytemplate/`.
--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
 from setuptools import setup, find_packages
 
+DESCRIPTION = """
+Route urls to file system templates in Django
+
+See:
+
+https://github.com/prestontimmons/django-filepages
+"""
+
+
 setup(
     name="django-filepages",
-    version="1.0",
-    long_description=__doc__,
+    version="1.0.0",
+    author="Preston Timmons",
+    author_email="prestontimmons@gmail.com",
+    url="https://github.com/prestontimmons/django-filepages",
+    description="Route urls to file system templates in Django.",
+    long_description=DESCRIPTION,
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
+    install_requires=[],
 )
