6b918afde01147e827df10043bcf7892dc5605f4
Preston Timmons <prestontimmons@gfa.org>
2014-10-12 21:08:58 +0000

    Updated test suite for Django 1.7.

--- runtests.py
+++ runtests.py
@@ -8,29 +8,37 @@ from django.conf import settings
 
 if not settings.configured:
     settings_dict = dict(
-        INSTALLED_APPS=["filepages", "test_filepages"],
+        INSTALLED_APPS=["filepages", ],
         DATABASES={
             "default": {
                 "ENGINE": "django.db.backends.sqlite3",
             },
         },
+        SILENCED_SYSTEM_CHECKS=["1_7.W001"],
     )
 
     settings.configure(**settings_dict)
 
 
-def runtests():
-    test_args = ["test_filepages"]
+import django
+django.setup()
+
+
+def runtests(test_labels):
     sys.path.insert(0, dirname(abspath(__file__)))
 
-    from django.test.simple import DjangoTestSuiteRunner
-    failures = DjangoTestSuiteRunner(
+    from django.test.runner import DiscoverRunner
+    failures = DiscoverRunner(
         verbosity=1,
         interactive=True,
         failfast=False,
-    ).run_tests(test_args)
+    ).run_tests(test_labels)
     sys.exit(failures)
 
 
 if __name__ == "__main__":
-    runtests()
+    labels = sys.argv[1:] or [
+        "filepages",
+    ]
+
+    runtests(labels)
