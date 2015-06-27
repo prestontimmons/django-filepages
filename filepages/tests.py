9d10891e5ba3afceb4c23a646bb311b72542fb74
Preston Timmons <prestontimmons@gmail.com>
2015-06-27 00:12:03 -0500

    Updated tests for Django 1.8.

--- filepages/tests.py
+++ filepages/tests.py
@@ -1,48 +1,50 @@
 # coding=utf-8
 
 from django.http import Http404
-from django.test import TestCase
-from django.test.client import RequestFactory
-from django.test.utils import (
-    restore_template_loaders,
-    setup_test_template_loader,
-)
+from django.test import RequestFactory, TestCase, override_settings
 
 from filepages.views import is_safe, page
 
 
+@override_settings(
+    TEMPLATES=[{
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "OPTIONS": {
+            "loaders": [
+                ("django.template.loaders.locmem.Loader", {
+                    "404.html": "",
+                    "filepages/about.html": "about",
+                    "filepages/base.html": "base",
+                    "filepages/sub/contact.html": "contact",
+                    "filepages/get-involved/index.html": "index",
+                    "other/filepages/other.html": "other",
+                }),
+            ],
+        },
+    }],
+)
 class PageViewTest(TestCase):
-
-    def setUp(self):
-        setup_test_template_loader({
-            "404.html": "",
-            "filepages/about.html": "",
-            "filepages/base.html": "",
-            "filepages/sub/contact.html": "",
-            "filepages/get-involved/index.html": "",
-            "other/filepages/other.html": "",
-        })
-
-    def tearDown(self):
-        restore_template_loaders()
 
     def test_path(self):
         request = RequestFactory().get("/about/")
         response = page(request, "/about/", directory="filepages/")
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "filepages/about.html")
+        response.render()
+        self.assertEqual(response.content.decode("utf-8"), "about")
 
     def test_directory_path(self):
         request = RequestFactory().get("/sub/contact/")
         response = page(request, "/sub/contact/", directory="filepages/")
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "filepages/sub/contact.html")
+        response.render()
+        self.assertEqual(response.content.decode("utf-8"), "contact")
 
     def test_index(self):
         request = RequestFactory().get("/get-involved/")
         response = page(request, "/get-involved/", directory="filepages/")
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "filepages/get-involved/index.html")
+        response.render()
+        self.assertEqual(response.content.decode("utf-8"), "index")
 
     def test_multiple_directories(self):
         request = RequestFactory().get("/other/")
@@ -51,7 +53,8 @@ class PageViewTest(TestCase):
             "other/filepages/",
         ])
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "other/filepages/other.html")
+        response.render()
+        self.assertEqual(response.content.decode("utf-8"), "other")
 
     def test_404(self):
         request = RequestFactory().get("/does-not-exist/")
